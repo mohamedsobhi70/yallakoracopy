23c33c5a4ad1eb9045582ffd650d96e452d1743b
mohammedsobhi70 <sobhi7070@gmail.com>
2021-11-23 11:11:09 +0200

    first initialize

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(".owl-carousel").owlCarousel({
+  rtl: true,
+  loop: true,
+  margin: 20,
+  nav: true,
+  autoplay: true,
+  autoplayTimeout: 3500,
+  dots: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+document.querySelectorAll("main .owl-prev").forEach((el) => {
+    el.innerHTML =`<i class="fa fa-chevron-left"></i>`;
+  });
+  
+  document.querySelectorAll("main .owl-next").forEach((el) => {
+    el.innerHTML = `<i class="fa fa-chevron-right"></i>`;
+  });
+  
